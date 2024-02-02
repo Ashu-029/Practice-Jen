2e0bf31f99a4ac4da1fb3d188c971e99e1fa3071
Ashu-019 <ashishsawle29@gmail.com>
2024-02-02 13:51:40 +0530

    added file

--- src/main/java/com/jenkins/PracticeJenApplication.java
+++ src/main/java/com/jenkins/PracticeJenApplication.java
@@ -1,12 +1,30 @@
 package com.jenkins;
 
+
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class PracticeJenApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(PracticeJenApplication.class);
+
+	@PostConstruct
+	public void init() {
+		
+		log.info(" Application Has Started sucessully for new instance......... ");
+		
+	}
+	
 	public static void main(String[] args) {
+		
+		log.info(" Application Has Started......... "); 
 		SpringApplication.run(PracticeJenApplication.class, args);
 	}
 
